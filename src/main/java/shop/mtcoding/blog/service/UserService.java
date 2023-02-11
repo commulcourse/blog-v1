7ee60c89799ff13b55398f708d0df0fe4f6a9787
commulcourse <commulcourse@gmail.com:git config --golbal user.email commulcourse@gmail.comgit config --global user.email commulcourse@gmail.com>
2023-02-11 22:27:18 +0900

    회원가입 서비스,레파지토리 완료

--- src/main/java/shop/mtcoding/blog/service/UserService.java
+++ src/main/java/shop/mtcoding/blog/service/UserService.java
@@ -5,12 +5,24 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import shop.mtcoding.blog.dto.user.UserReq.JoinReqDto;
+import shop.mtcoding.blog.handler.ex.CustomException;
+import shop.mtcoding.blog.model.User;
+import shop.mtcoding.blog.model.UserRepository;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Transactional
     public int 회원가입(JoinReqDto joinReqDto) {
-        return -1;
+        User sameUser = userRepository.findByUsername(joinReqDto.getUsername());
+        if (sameUser != null) {
+            throw new CustomException("동일한 username이 존재합니다");
+        }
+        int result = userRepository.insert(joinReqDto.getUsername(), joinReqDto.getPassword(), joinReqDto.getEmail());
+        return result;
+        // return -1;
     }
 }
